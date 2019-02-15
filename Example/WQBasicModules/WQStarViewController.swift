f090920274d509a7b4a4c38f4bdf9d12a69233bb
wang68543 <wang68543@163.com>
2019-02-15 11:18:30 +0800

    优化 并完成星星控件

--- Example/WQBasicModules/WQStarViewController.swift
+++ Example/WQBasicModules/WQStarViewController.swift
@@ -13,26 +13,16 @@ class WQStarViewController: BaseExampleViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let star = WQStarControl()
-        star.value = 90
+        star.value = 0.5
 //        star.normalImage = UIImage(named: "003")
 //        star.halfHighlightedImage = UIImage(named: "002")
 //        star.highlightedImage = UIImage(named: "001")
+//        star.hideUnHighlited = true
         star.starSize = CGSize(width: 36, height: 33)
         star.valueType = .valueRandom
         star.backgroundColor = .white
         star.frame = CGRect(x: 30, y: 400, width: 300, height: 80)
         self.view.addSubview(star)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- WQBasicModules/Classes/WQUI/Custom/WQStarControl.swift
+++ WQBasicModules/Classes/WQUI/Custom/WQStarControl.swift
@@ -10,75 +10,64 @@ public enum WQStarValueType {
     case valueHalf, valueWhole, valueRandom // Random is not support custom image
 }
 open class WQStarControl: UIControl {
-    /// 0.0 ~ 1.0
-    private var progressValue: CGFloat = 0.0 {
+    
+    public var minimumValue: Int = 0
+    public var maximumValue: Int = 1
+    /// minimumValue ~ maximumValue
+    public var value: CGFloat = 0 {
         didSet {
-            if progressValue != oldValue {
-              self.setNeedsDisplay()
+            guard value != oldValue else {
+                return
             }
+            self.setNeedsDisplay()
         }
     }
-    //是否连续产生事件
-    public var isContinuous: Bool = true
-    public var minimumValue: Int = 0
-    public var maximumValue: Int = 100
-    /// 0 ~ 100
-    public var value: Int {
-            set {
-                let starProgress = CGFloat(min(max(minimumValue, newValue), maximumValue)) / CGFloat(maximumValue - minimumValue)
-                self.progressValue = starProgress
-            }
-            get {
-                let contentRect = self.frame.inset(by: self.contentEdgeInsets)
-                let progressX = contentRect.width * self.progressValue
-                let includeStar = floor(progressX / (starSize.width + minInterSpacing))
-                var progress = (progressX - (starSize.width + minInterSpacing) * includeStar) / starSize.width
-                progress = min(progress, 1.0)
-                if progress > 0.0 {
-                    switch valueType {
-                    case .valueHalf:
-                        progress = progress > 0.5 ? 1.0 : 0.5
-                    case .valueWhole:
-                        progress = 1.0
-                    default:
-                        break
-                    }
-                }
-                return Int(CGFloat(maximumValue - minimumValue) / CGFloat(starCount) * (progress + includeStar)) + minimumValue
-            }
-    }
-    
-    public var minInterSpacing: CGFloat = 5
+    /// 这里如果是zero的话 会根据当前frame 动态计算出合适的尺寸
+    public var starSize: CGSize  = .zero
+    /// polygon counts
+    public var counts: Int = 5
     
     public var valueType: WQStarValueType = .valueHalf
-    public var contentEdgeInsets: UIEdgeInsets = .zero
-    /// 旋转角度
-    public var drawStarRotate: CGFloat = 0.0
-    public var starSize: CGSize  = .zero
- 
-    public var hideUnHighlited = false
-    ///默认形状(五角形)
-    public var shapeCoreners: Int = 4
-    public var starCount: Int = 5
     
-    public var normalImage: UIImage?
-    public var halfHighlightedImage: UIImage?
-    public var highlightedImage: UIImage?
- 
+    ///绘制的图形的角的个数
+    public var shapeCoreners: Int = 5 {
+        didSet {
+            assert(shapeCoreners >= 3, "不支持小于三个角的图形绘制")
+        }
+    }
     public var normalColor: UIColor = .clear
     public var highlightedColor: UIColor = .red
     public var borderWidth: CGFloat = 1.0
     public var borderColor: UIColor = .red
     
+    public var normalImage: UIImage?
+    public var halfHighlightedImage: UIImage?
+    public var highlightedImage: UIImage?
+   
+    /// 每个星星之间的最小间距
+    public var minInterSpacing: CGFloat = 5
+    public var contentEdgeInsets: UIEdgeInsets = .zero
+    /// 旋转角度
+    public var drawStarRotate: CGFloat = 0.0
+    /// 不亮的星星是否不显示
+    public var hideUnHighlited = false
+    ///是否连续产生事件 为false的时候 只有当结束了才会产生事件
+    public var isSendActionContinuous: Bool = true
+    
+    private var rects: [CGRect] = []
+    /// 是否是使用图片绘制
+    private var isUsingImage: Bool = false
+    
     open override func layoutSubviews() {
         super.layoutSubviews()
         let contentRect = self.frame.inset(by: self.contentEdgeInsets)
+        let count = CGFloat(counts)
         if self.starSize == .zero {
             if let normalImage = normalImage,
                 highlightedImage != nil {
                 self.starSize = normalImage.size
             } else {
-                let itemW = min((contentRect.width - minInterSpacing * CGFloat(starCount - 1)) / CGFloat(starCount), contentRect.height)
+                let itemW = min((contentRect.width - minInterSpacing * (count - 1)) / count, contentRect.height)
                 starSize = CGSize(width: itemW, height: itemW)
             }
         }
@@ -96,10 +85,10 @@ open class WQStarControl: UIControl {
             }
             switch self.contentHorizontalAlignment {
             case .center, .fill:
-                edge.left = (self.frame.width - starSize.width * CGFloat(starCount) - self.minInterSpacing * CGFloat(starCount - 1)) * 0.5
+                edge.left = (self.frame.width - starSize.width * count - self.minInterSpacing * (count - 1)) * 0.5
                 edge.right = edge.left
             case .right:
-                edge.left = (self.frame.width - starSize.width * CGFloat(starCount) - self.minInterSpacing * CGFloat(starCount - 1))
+                edge.left = (self.frame.width - starSize.width * count - self.minInterSpacing * (count - 1))
                 edge.right = 0
             default:
                 break
@@ -110,39 +99,29 @@ open class WQStarControl: UIControl {
             self.starSize.width <= contentRect.width else {
                 debugPrint("星星的尺寸必须小于控件的尺寸");return 
         }
-        if normalImage != nil && highlightedImage != nil {
-            if halfHighlightedImage != nil {
-                if valueType == .valueRandom {
-                    valueType = .valueHalf
-                }
-            } else {
-                if valueType != .valueWhole {
-                    valueType = .valueWhole
-                }
-            }
-        }
+        //更新绘制方式
+        shouldUpdateDrawMethod()
     }
     override open func draw(_ rect: CGRect) {
         super.draw(rect)
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return
-        }
+        guard let context = UIGraphicsGetCurrentContext() else { return }
         context.clear(rect)
         context.setFillColor(self.backgroundColor?.cgColor ?? UIColor.clear.cgColor)
         context.fill(rect)
         
-        let top = self.contentEdgeInsets.top 
-        for idx in 0 ..< starCount {
+        let top = self.contentEdgeInsets.top
+        var items: [CGRect] = []
+        for idx in 0 ..< counts {
             let starPoint = CGPoint(x: self.contentEdgeInsets.left + (minInterSpacing + starSize.width) * CGFloat(idx), y: top)
             let starRect = CGRect(origin: starPoint, size: starSize)
-            drawItem(starRect, context: context)
+            items.append(starRect)
+            drawItem(starRect, index: idx, context: context)
         }
+        rects = items
     }
     open override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         super.beginTracking(touch, with: event)
-        if !self.isFirstResponder {
-            self.becomeFirstResponder()
-        }
+        if !self.isFirstResponder { self.becomeFirstResponder() }
         handleTouch(touch)
         return true
     }
@@ -153,9 +132,7 @@ open class WQStarControl: UIControl {
     }
     open override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
         super.endTracking(touch, with: event)
-        if self.isFirstResponder {
-            self.resignFirstResponder()
-        }
+        if self.isFirstResponder { self.resignFirstResponder() }
         if let touch = touch {
             handleTouch(touch, isEnd: true)
         }
@@ -165,21 +142,77 @@ open class WQStarControl: UIControl {
         return true
     }
 }
+private extension WQStarControl {
+    /// 每个星星的分值
+    var starValue: CGFloat {
+        return CGFloat(self.maximumValue - self.minimumValue) / CGFloat(counts)
+    }
+    /// 根据坐标点获取进度
+    func value(for point: CGPoint) -> CGFloat {
+        guard !rects.isEmpty else { return 0.0 }
+        var minX: CGFloat = 0.0
+        var maxIndex: CGFloat = -1
+        var rectW: CGFloat = 0
+        for rect in rects {
+            if rect.minX < point.x {
+                minX = rect.minX
+                rectW = rect.width
+                maxIndex += 1
+            } else {
+                break
+            }
+        }
+        if maxIndex < 0 {
+            return 0.0
+        } else {
+            return (maxIndex + min(1, (point.x - minX) / rectW)) * starValue
+        }
+    }
+    // 计算每个星星的刻画进度
+    func progress(for index: Int) -> CGFloat {
+        let count = value / starValue
+        let idx = CGFloat(index)
+        let delt = count - idx
+        var progress = min(1.0, max(delt, 0))
+        switch self.valueType {
+        case .valueWhole:
+            progress = ceil(progress) // 向上取整
+        case .valueHalf:
+            if progress != 0 {
+                progress = progress <= 0.5 ? 0.5 : 1
+            }
+        default: break
+        }
+        return progress
+    }
+    //根据外界设置的图片来判定是否使用图片绘制
+    func shouldUpdateDrawMethod() {
+        guard normalImage != nil else {
+            self.isUsingImage = false
+            return
+        }
+        guard highlightedImage != nil else {
+            self.isUsingImage = false
+            return
+        }
+        self.isUsingImage = true
+        guard halfHighlightedImage != nil else {
+            valueType = .valueWhole
+            return
+        }
+        if valueType == .valueRandom { // 不支持random
+            valueType = .valueHalf
+        }
+    }
+}
 /// 画单个
 private extension WQStarControl {
+    
     func handleTouch(_ touch: UITouch, isEnd: Bool = false) {
         let point = touch.location(in: self)
-        var progress: CGFloat
-        if point.x <= self.contentEdgeInsets.left {
-            progress = 0.0
-        } else if point.x >= self.frame.width - self.contentEdgeInsets.right {
-            progress = 1.0
-        } else {
-            let contentRect = self.frame.inset(by: self.contentEdgeInsets)
-            progress = (point.x - self.contentEdgeInsets.left) / contentRect.width
-        }
-        self.progressValue = progress
-        if isContinuous {
+        self.value = value(for: point)
+        guard isEnabled else { return }
+        if isSendActionContinuous {
            self.sendActions(for: .valueChanged)
         } else {
             if isEnd {
@@ -187,36 +220,20 @@ private extension WQStarControl {
             }
         }
     }
-    func drawItem(_ rect: CGRect, context: CGContext) {
-        let contentRect = self.frame.inset(by: self.contentEdgeInsets)
-        var progress: CGFloat
-        let progressX = contentRect.width * self.progressValue + self.contentEdgeInsets.left
-        
-        if progressX >= rect.maxX {
-            progress = 1.0
-        } else if progressX <= rect.minX {
-            progress = 0.0
-        } else {
-            progress = (progressX - rect.minX) / rect.width
-            switch valueType {
-            case .valueHalf:
-                progress = progress > 0.5 ? 1.0 : 0.5
-            case .valueWhole:
-                progress = ceil(progress)
-            default:
-                break
-            }
-        }
-        if  normalImage != nil && highlightedImage != nil {
+    func drawItem(_ rect: CGRect, index: Int, context: CGContext) {
+        let progress = self.progress(for: index)
+        guard !self.hideUnHighlited || progress > 0 else { return } // 隐藏不亮的星星
+        if  self.isUsingImage {
             drawImage(rect, progress: progress, context: context)
         } else {
             drawStar(rect, progress: progress, context: context)
         }
   }
     // 算法参照: https://blog.csdn.net/djh123456021/article/details/78306250
+    //swiftlint:disable function_body_length
     func drawStar(_ rect: CGRect, progress: CGFloat, context: CGContext) {
         let count = CGFloat(shapeCoreners)
-        let degree = 180 / count
+        let degree = 180 / count // 内角和
         context.saveGState()
         let splitAngle = 360.0 / count // 圆上点的分割弧度
         let radius = min(rect.width, rect.height) * 0.5 // 外角半斤
@@ -235,12 +252,20 @@ private extension WQStarControl {
         }
         let path = UIBezierPath()
         path.move(to: pts.first!)
-        for index in 0 ..< shapeCoreners { // 绘制五角星
-            let idx = (index + 1) % pts.count
-            let pt2 = pts[idx]
-            let midPt = internalPts[index]
-            path.addLine(to: midPt)
-            path.addLine(to: pt2)
+        if shapeCoreners == 3 {
+            for index in 0 ..< shapeCoreners { // special for triangle
+                let idx = (index + 1) % pts.count
+                let pt2 = pts[idx]
+                path.addLine(to: pt2)
+            }
+        } else {
+            for index in 0 ..< shapeCoreners { // draw polygon
+                let idx = (index + 1) % pts.count
+                let pt2 = pts[idx]
+                let midPt = internalPts[index]
+                path.addLine(to: midPt)
+                path.addLine(to: pt2)
+            }
         }
         path.close()
         path.addClip()
