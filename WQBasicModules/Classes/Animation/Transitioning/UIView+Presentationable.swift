6fa59dab1a3b0ee41e849b333086ffb20c369d6a
wang68543 <wang68543@163.com>
2019-07-05 18:11:47 +0800

    新增 跳转

--- WQBasicModules/Classes/Animation/Transitioning/UIView+Presentationable.swift
+++ WQBasicModules/Classes/Animation/Transitioning/UIView+Presentationable.swift
@@ -27,12 +27,28 @@ public extension WQModules where Base: UIView {
 
 // MARK: - normal animate api
 public extension WQModules where Base: UIView {
-    /// 动画参数配置完成之后展示 内部没有强引用 需要外部强引用了presention 否则没效果
+    /// 在页面show之后此处才有值 动画参数配置完成之后展示 内部没有强引用 需要外部强引用了presention 否则没效果
     internal func present(in viewController: UIViewController?, completion: (() -> Void)? = nil) {
         //使用下划线保存的返回变量 会在返回的时候就销毁了
         if let presention = self.presentation {
             presention.show(animated: true, in: viewController, completion: completion)
         }
+    }
+    /// 自己直接设置动画尺寸
+    func show(from: CGRect,
+              show: CGRect,
+              dismiss: CGRect? = nil,
+              isDimming: Bool = true,
+              inController: UIViewController? = nil,
+              completion: (() -> Void)? = nil) {
+        let item = WQAnimatedItem(containerFrame: from, show: show, dismiss: dismiss)
+        var items: [WQAnimatedConfigAble] = []
+        items.append(item)
+        if isDimming {
+            let dimmingBakcground = WQAnimatedItem.defaultViewBackground()
+            items.append(dimmingBakcground)
+        }
+        self.show(items: items, inController: inController, completion: completion)
     }
     /// 采用默认的动画风格展示
     func show(from: WQPresentationOption.Position,
