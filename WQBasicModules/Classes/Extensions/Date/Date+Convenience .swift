7444f9aa2bb99e092c77ffdb6476131cf6c6e39c
wang68543 <wang68543@163.com>
2019-08-19 08:53:52 +0800

    提交修改

---
+++ WQBasicModules/Classes/Extensions/Date/Date+Convenience .swift
@@ -0,0 +1,19 @@
+//
+//  Date+Convenience .swift
+//  Pods
+//
+//  Created by iMacHuaSheng on 2019/8/15.
+//
+
+import Foundation
+public extension Int {
+//    var secondsDate: Date {
+//        return Date
+//    }
+}
+public extension Double {
+    
+}
+public extension Float {
+    
+}
