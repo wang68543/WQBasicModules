a1f8cd4759f6c8e205aa7ea14be4f8891f2b21ac
wang68543 <wang68543@163.com>
2020-09-23 18:36:54 +0800

    修改动画

--- Sources/Animation/TransitionStyle/ModalDefaultAnimation.swift
+++ Sources/Animation/TransitionStyle/ModalDefaultAnimation.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 public class ModalDefaultAnimation: ModalBaseAnimation {
+    public func prepare(show manager: TransitionManager, states: WQReferenceStates, completion: @escaping Completion) {
+         
+    }
+    
+    public func show(_ manager: TransitionManager, states: WQReferenceStates, completion: @escaping Completion) {
+         
+    }
+    
+    public func prepare(hide manager: TransitionManager, states: WQReferenceStates, completion: @escaping Completion) {
+         
+    }
+    
+    public func hide(_ manager: TransitionManager, states: WQReferenceStates, completion: @escaping Completion) {
+         
+    }
+    
     
 }
