23746c61674e49db4e31e5366720f770bd24233d
wang68543 <wang68543@163.com>
2020-12-08 18:33:19 +0800

    添加 style 设置

--- Sources/Extensions/UIKit/UINavigationBar+Style.swift
+++ Sources/Extensions/UIKit/UINavigationBar+Style.swift
@@ -17,6 +17,7 @@ public struct UINavigationBarStyle: RawRepresentable, Equatable {
 }
 public extension UINavigationBarStyle {
     static let none = UINavigationBarStyle(rawValue: "none")
+    /// 透明
     static let translucent = UINavigationBarStyle(rawValue: "translucent")
 }
 
@@ -40,6 +41,11 @@ public extension UINavigationBar {
     /// 用于 保存当前UINavigationBar 的自定义风格 便于比较
     var style: UINavigationBarStyle {
         set {
+            let oldValue = objc_getAssociatedObject(self, AssociatedKeys.styleRawValue) as? UINavigationBarStyle ?? .none
+            guard oldValue != newValue else { return }
+            if newValue == .translucent {
+                self.makeTransparent()
+            }
             objc_setAssociatedObject(self, AssociatedKeys.styleRawValue, newValue, .OBJC_ASSOCIATION_COPY_NONATOMIC)
         }
         get {
