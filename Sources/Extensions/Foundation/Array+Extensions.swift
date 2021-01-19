5e6609339254bd497b8161c0495c3b17c73e8d75
wang68543 <wang68543@163.com>
2021-01-19 11:08:28 +0800

    添加数组排序

---
+++ Sources/Extensions/Foundation/Array+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Array+Extensions.swift
+//  Pods
+//
+//  Created by iMacHuaSheng on 2021/1/19.
+//
+
+import Foundation
+public extension Array {
+    mutating func sort<T: Comparable>(
+      by keyPath: KeyPath<Element, T>,
+      criteria: (T, T) -> Bool = { $0 < $1 }) {
+      sort { a, b in
+        criteria(a[keyPath: keyPath], b[keyPath: keyPath])
+      }
+    }
+
+    func sorted<T: Comparable>(
+      by keyPath: KeyPath<Element, T>,
+      criteria: (T, T) -> Bool = { $0 < $1 }) -> [Element] {
+      return sorted { a, b in
+        criteria(a[keyPath: keyPath], b[keyPath: keyPath])
+      }
+    }
+}
