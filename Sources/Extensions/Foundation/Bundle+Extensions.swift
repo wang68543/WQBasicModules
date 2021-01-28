43b54951d95817579eafd221598975246c4a3b6b
wang68543 <wang68543@163.com>
2021-01-28 18:16:59 +0800

    修改文件

---
+++ Sources/Extensions/Foundation/Bundle+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Bundle+Extensions.swift
+//  Pods
+//
+//  Created by iMacHuaSheng on 2021/1/26.
+//
+
+import Foundation
+public extension Bundle {
+    func image(for named: String, ofType type: String? = nil) -> UIImage? {
+        guard let path = self.path(forResource: named, ofType: type) else {
+            return nil
+        }
+        return UIImage(contentsOfFile: path)
+    }
+}
