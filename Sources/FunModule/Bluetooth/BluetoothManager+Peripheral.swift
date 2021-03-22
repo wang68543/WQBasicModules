b349711fa7c806c171f9ba0086a9b719d2bd8a62
wang68543 <wang68543@163.com>
2021-03-22 16:34:07 +0800

    蓝牙 模块待完善

--- Sources/FunModule/Bluetooth/BluetoothManager+Peripheral.swift
+++ Sources/FunModule/Bluetooth/BluetoothManager+Peripheral.swift
@@ -11,6 +11,25 @@ import CoreBluetooth
 public extension BluetoothManager {
     
     
+    func connect(_ peripheral: CBPeripheral, options: [String: Any]? = nil) {
+        // TODO: 系统默认没有连接超时 如果自定义实现连接超时 需要手动调用cancelPeripheralConnection
+        switch peripheral.state {
+        case .connected, .connecting:
+            self.peripheral(peripheral, stateDidChange: peripheral.state)
+            //TODO: 需设置代理
+        default:
+            guard self.centralMgr.state == .poweredOff else {
+                self.peripheral(peripheral, stateDidChange: .disconnected)
+                //连接失败
+                return
+            }
+            objc_sync_enter(self)
+            self.centralMgr.connect(peripheral, options: options)
+            connectedPeripherals.append(peripheral)
+            objc_sync_exit(self)
+        }
+    }
+    
     /// 重连设备
     /// - Parameters:
     ///   - peripheral: 设备的唯一标识
@@ -30,20 +49,60 @@ public extension BluetoothManager {
             }
         }
         if let dev = device {
-            centralMgr.connect(dev, options: nil)
+            self.connect(dev, options: nil)
         } else {
             //扫描
+            scan(nil, options: nil, peripheral: peripheral)
         }
     }
     
-    func scan(_ services: [CBUUID]?, options: [String: Any]) {
-        centralMgr.scanForPeripherals(withServices: services, options: options)
+    /// 扫描设备
+    /// - Parameters:
+    ///   - services: 设备的服务
+    ///   - options: 扫描设备的参数
+    ///   - peripheral: 自动连接的设备名称
+    func scan(_ services: [CBUUID]?, options: [String: Any]?, peripheral: UUID? = nil) {
+        objc_sync_enter(self)
+        self.waitToConnectPeripheral = peripheral?.uuidString
+        if centralMgr.state != .poweredOn {
+            self.waitBLePoweredOnToScan = true
+        } else {
+            self.waitBLePoweredOnToScan = false
+            if !centralMgr.isScanning {
+                centralMgr.scanForPeripherals(withServices: services, options: options)
+            }
+        }
+        objc_sync_exit(self)
     }
+    // 设备连接的状态改变了
+    func peripheral(_ peripheral: CBPeripheral, stateDidChange state: CBPeripheralState) {
+        DispatchQueue.main.async {
+            NotificationCenter.default.post(name: BluetoothManager.peripheralStateDidChange, object: peripheral, userInfo: nil)
+        }
+    }
+    
 }
 extension BluetoothManager: CBCentralManagerDelegate {
  
     public func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        
+        switch central.state {
+        case .poweredOn:
+            let identifier = UUID(uuidString: self.waitToConnectPeripheral ?? "")
+            if self.waitBLePoweredOnToScan || identifier != nil {
+                self.scan(nil, options: nil, peripheral: identifier)
+            }
+        case .poweredOff:
+            // TODO: 测试流程
+            
+            // 查看设备的断开
+        break
+        case .unauthorized:
+            // TODO: 等待验证
+            break
+        default:
+            debugPrint("===开关异常")
+            break
+        }
     }
 
     
@@ -53,23 +112,36 @@ extension BluetoothManager: CBCentralManagerDelegate {
 
     
     public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        
+        if let identifier = self.waitToConnectPeripheral,
+           let uuid = UUID(uuidString: identifier) {
+            if peripheral.identifier == uuid {
+                central.stopScan()
+                self.connect(peripheral, options: nil)
+            }
+        } else {
+            NotificationCenter.default.post(name: BluetoothManager.didReceivedAdvertisement, object: peripheral, userInfo: ["data": advertisementData, "rssi": RSSI])
+        }
     }
-
+    // connect api success
     public func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        
+        // 取消超时等待
     }
 
-    
+    // connect api failed
     public func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-        
+        // 取消超时等待
     }
-
+    // Deallocating peripheral 解除分配也会引起 cancelPeripheralConnection
     public func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        
+        if error == nil { // 用户主动断开连接的
+            // 发通知
+        } else {
+            let services: [CBUUID] = peripheral.services?.map(\.uuid) ?? []
+            retrievePeripheral(peripheral.identifier, services: services)
+        }
     }
 
-    
+    @available(iOS 13.0, *)
     public func centralManager(_ central: CBCentralManager, connectionEventDidOccur event: CBConnectionEvent, for peripheral: CBPeripheral) {
         
     } 
--- Sources/FunModule/Bluetooth/BluetoothManager.swift
+++ Sources/FunModule/Bluetooth/BluetoothManager.swift
@@ -5,12 +5,27 @@
 //  Created by iMacHuaSheng on 2021/3/19.
 //  Copyright © 2021 CocoaPods. All rights reserved.
 //
-
+//  官方蓝牙文档
+//  https://developer.apple.com/library/archive/documentation/NetworkingInternetWeb/Conceptual/CoreBluetooth_concepts/AboutCoreBluetooth/Introduction.html#//apple_ref/doc/uid/TP40013257-CH1-SW1
 import Foundation
 import CoreBluetooth
 open class BluetoothManager: NSObject {
-    lazy var centralMgr: CBCentralManager = {
+    public lazy var centralMgr: CBCentralManager = {
         let mgr = CBCentralManager(delegate: nil, queue: nil)
         return mgr
     }()
+     
+    open var connectedPeripherals: [CBPeripheral] = []
+    
+    /// 等待自动连接的设备 (这个设备必定是之前自动连接过的)
+    internal var waitToConnectPeripheral: String?
+    /// 等待
+    internal var waitBLePoweredOnToScan: Bool = false
+    
+}
+public extension BluetoothManager {
+    /// 设备的连接状态改变
+    static let peripheralStateDidChange = Notification.Name(rawValue: "peripheralStateDidChangeNotification")
+    /// 接收到设备的广播通知
+    static let didReceivedAdvertisement = Notification.Name(rawValue: "didReceivedAdvertisementNotification")
 }
